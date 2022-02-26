8782757bbc0caca964a3277054f7bd70f1bbe756
enw12345 <1evan.williams1@gmail.com>
2022-02-26 15:59:15 -0500

    Done with character controller

---
+++ Assets/Scripts/CameraBehaviour/CameraLook.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CameraBehaviour
+{
+    public class CameraLook : MonoBehaviour
+    {
+        //Rotation
+        [SerializeField] [Range(1,5)] private float lookSpeed = 2f;
+        [SerializeField] [Range(1, 100)] private float sensitivity = 100f;
+        private float _xRotation = 0f;
+
+        private void Update()
+        {
+            Look();
+        }
+
+        private void Look()
+        {
+            var mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+            
+            _xRotation -= mouseY;
+            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterBehaviour/FirstPersonController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+namespace CharacterBehaviour
+{
+    public class FirstPersonController : MonoBehaviour
+    {
+        private Transform _transform;
+
+        private CapsuleCollider _collider;
+
+        public LayerMask groundLayer;
+        
+        //Movement
+        [SerializeField] private CharacterController controller;
+        [SerializeField] [Range(1, 25)] private float moveSpeed = 12f;
+
+        private Vector3 velocity;
+
+        [SerializeField] [Range(1,5)] private float lookSpeed = 2f;
+        [SerializeField] [Range(1, 100)] private float sensitivity = 100f;
+
+        private float _yRotation = 0f;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _transform = GetComponent<Transform>();
+            _collider = GetComponent<CapsuleCollider>();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            Move();
+            Look();
+        }
+
+        private void FixedUpdate()
+        {
+            print(IsGrounded());
+        }
+
+        private void Move()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+
+            var move = _transform.right * x + _transform.forward * z;
+            controller.Move(move * moveSpeed * Time.deltaTime);
+        }
+        private void Look()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+            _yRotation += mouseX;
+            transform.localRotation = Quaternion.Euler(0, _yRotation, 0);
+        }
+        
+        
+        private bool IsGrounded()
+        {
+            var colliderBounds = _collider.bounds;
+            var raycastHit = Physics.Raycast(colliderBounds.center, Vector3.down, colliderBounds.extents.y + 0.1f, groundLayer);
+            var rayColor = raycastHit ? Color.green : Color.red;
+            Debug.DrawRay(colliderBounds.center, Vector3.down * (colliderBounds.extents.y + 1), rayColor);
+            return raycastHit;
+        }
+    }
+}
