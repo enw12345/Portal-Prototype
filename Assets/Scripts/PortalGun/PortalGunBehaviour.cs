7f0cb0dadaea28573539c07d73c4c78464cb13d0
enw12345 <1evan.williams1@gmail.com>
2022-02-27 19:16:06 -0500

    Getting started building the level

--- Assets/Scripts/PortalGun/PortalGunBehaviour.cs
+++ Assets/Scripts/PortalGun/PortalGunBehaviour.cs
@@ -4,6 +4,26 @@ namespace PortalGun
 {
     public class PortalGunBehaviour : MonoBehaviour
     {
-        
+        [SerializeField] private Transform _portalSpawner;
+        [SerializeField] private PortalProjectile _bluePortalProjectile;
+        [SerializeField] private PortalProjectile _orangePortalProjectile;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Fire(_bluePortalProjectile);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Fire(_orangePortalProjectile);
+            }
+        }
+
+        private void Fire(PortalProjectile projectile)
+        {
+            Instantiate(projectile, _portalSpawner.position, _portalSpawner.rotation);
+        }
     }
 }
---
+++ Assets/Scripts/PortalGun/PortalProjectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PortalGun
+{
+    public class PortalProjectile : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Rigidbody _rigidbody;
+        
+        private void OnEnable()
+        {
+            _rigidbody.velocity = transform.forward * _speed;
+        }
+    }
+}
